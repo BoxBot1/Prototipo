b1a7869ea2bd838e80f76afdfc05b8f249bba326
BoxBot1 <40348403+BoxBot1@users.noreply.github.com>
2018-06-24 21:25:00 -0500

    Update BoxBot.py

--- BoxBot.py
+++ BoxBot.py
@@ -8,125 +8,148 @@ serial_port = '/dev/tty.BoxBotnny-SPPDev'
 def Traer_caja():
     ser.write(b'1')
 
-def Devolver_caja():
-    ser.write(b'2')
-
-def Reposo():
-    ser.write(b'3')
-
 print("Conectando al serial")
 ser = serial.Serial(serial_port, serial_speed, timeout=1)
 
+def Traer_caja():
+	print("-")
+
 master = Tk()
 
-texto = Label(master, text = "BoxBot 1.0",height=1, width=15 )
+texto = Label(master, text = "BoxBot 1.0",height=1, width=15 , font="Times 40")
 texto.pack()
-texto.place(x=400, y=10)
+texto.place(x=150, y=10)
 
-caja1 = Label(master, text = "Caja 1")
+caja1 = Label(master, text = "Caja 1:")
 caja1.pack()
-caja1.place(x=25, y=50)
+caja1.place(x=25, y=100)
 
 nombre1 = Entry(master)
-nombre1.place(x=100, y=50)
+nombre1.place(x=75, y=100)
 
 boton1 = Button(master, text = "Traer caja", height=1, width=15, command=Traer_caja)
-boton1.place(x=225, y=50)
+boton1.place(x=275, y=100)
 
 var1 = IntVar()
 c1 = Checkbutton(master, text = "Desactivar", variable=var1)
-c1.place(x=300, y=50)
+c1.place(x=450, y=100)
 
-caja2 = Label(master, text = "Caja 2")
-caja2.place(x=25, y=80)
+caja2 = Label(master, text = "Caja 2:")
+caja2.place(x=25, y=140)
 
 nombre2 = Entry(master)
-nombre2.place(x=100, y=80)
+nombre2.place(x=75, y=140)
 
 boton2 = Button(master, text = "Traer caja", height=1, width=15, command=Traer_caja)
-boton2.place(x=225, y=80)
+boton2.place(x=275, y=140)
 
 var2 = IntVar()
 c2 = Checkbutton(master, text = "Desactivar", variable=var2)
-c2.place(x=300, y=80)
+c2.place(x=450, y=140)
 
-caja3 = Label(self, text = "Caja 3")
-caja3.place(x=60, y=40)
+caja3 = Label(master, text = "Caja 3:")
+caja3.place(x=25, y=180)
 
-nombre3 = Entry(self)
-nombre3.place(x=60, y=40)
+nombre3 = Entry(master)
+nombre3.place(x=75, y=180)
 
-boton3 = Button(self, text = "Traer caja", height=1, width=15, command=Traer_caja)
-boton3.place(x=60, y=40)
+boton3 = Button(master, text = "Traer caja", height=1, width=15, command=Traer_caja)
+boton3.place(x=275, y=180)
 
 var3 = IntVar()
-c3 = Checkbutton(self, text = "Desactivar", variable=var3)
-c3.place(x=60, y=40)
+c3 = Checkbutton(master, text = "Desactivar", variable=var3)
+c3.place(x=450, y=180)
 
-caja4 = Label(self, text = "Caja 4")
-scaja4.place(x=60, y=40)
+caja4 = Label(master, text = "Caja 4:")
+caja4.place(x=25, y=220)
 
-nombre4 = Entry(self)
-nombre4.place(x=60, y=40)
+nombre4 = Entry(master)
+nombre4.place(x=75, y=220)
 
-boton4 = Button(self, text = "Traer caja", height=1, width=15, command=Traer_caja)
-boton4.place(x=60, y=40)
+boton4 = Button(master, text = "Traer caja", height=1, width=15, command=Traer_caja)
+boton4.place(x=275, y=220)
 
 var4 = IntVar()
-c4 = Checkbutton(self, text = "Desactivar", variable=var4)
-c4.place(x=60, y=40)
+c4 = Checkbutton(master, text = "Desactivar", variable=var4)
+c4.place(x=450, y=220)
 
-caja5 = Label(self, text = "Caja 5")
-caja5.place(x=60, y=40)
+caja5 = Label(master, text = "Caja 5:")
+caja5.place(x=25, y=260)
 
-nombre5 = Entry(self)
-nombre5.place(x=60, y=40)
+nombre5 = Entry(master)
+nombre5.place(x=75, y=260)
 
-boton5 = Button(self, text = "Traer caja", height=1, width=15, command=Traer_caja)
-boton5.place(x=60, y=40)
+boton5 = Button(master, text = "Traer caja", height=1, width=15, command=Traer_caja)
+boton5.place(x=275, y=260)
 
 var5 = IntVar()
-c5 = Checkbutton(self, text = "Desactivar", variable=var5)
-c5.place(x=60, y=40)
+c5 = Checkbutton(master, text = "Desactivar", variable=var5)
+c5.place(x=450, y=260)
 
-caja6 = Label(self, text = "Caja 6")
-caja6.place(x=60, y=40)
+caja6 = Label(master, text = "Caja 6:")
+caja6.place(x=25, y=300)
 
-nombre6 = Entry(self)
-nombre6.place(x=60, y=40)
+nombre6 = Entry(master)
+nombre6.place(x=75, y=300)
 
-boton6 = Button(self, text = "Traer caja", height=1, width=15, command=Traer_caja)
-boton6.place(x=60, y=40)
+boton6 = Button(master, text = "Traer caja", height=1, width=15, command=Traer_caja)
+boton6.place(x=275, y=300)
 
 var6 = IntVar()
-c6 = Checkbutton(self, text = "Desactivar", variable=var6)
-c6.place(x=60, y=40)
+c6 = Checkbutton(master, text = "Desactivar", variable=var6)
+c6.place(x=450, y=300)
 
-caja7 = Label(self, text = "Caja 7")
-caja7.place(x=60, y=40)
+caja7 = Label(master, text = "Caja 7:")
+caja7.place(x=25, y=340)
 
-nombre7 = Entry(self)
-nombre7.place(x=60, y=40)
+nombre7 = Entry(master)
+nombre7.place(x=75, y=340)
 
-boton7 = Button(self, text = "Traer caja", height=1, width=15, command=Traer_caja)
-boton7.place(x=60, y=40)
+boton7 = Button(master, text = "Traer caja", height=1, width=15, command=Traer_caja)
+boton7.place(x=275, y=340)
 
 var7 = IntVar()
-c7 = Checkbutton(self, text = "Desactivar", variable=var7)
-c7.place(x=60, y=40)
+c7 = Checkbutton(master, text = "Desactivar", variable=var7)
+c7.place(x=450, y=340)
 
-caja8 = Label(self, text = "Caja 8")
-caja8.place(x=60, y=40)
+caja8 = Label(master, text = "Caja 8:")
+caja8.place(x=25, y=380)
 
-nombre8 = Entry(self)
-nombre8.place(x=60, y=40)
+nombre8 = Entry(master)
+nombre8.place(x=75, y=380)
 
-boton8 = Button(self, text = "Traer caja", height=1, width=15, command=Traer_caja)
-boton8.place(x=60, y=40)
+boton8 = Button(master, text = "Traer caja", height=1, width=15, command=Traer_caja)
+boton8.place(x=275, y=380)
 
 var8 = IntVar()
-c8 = Checkbutton(self, text = "Desactivar", variable=var8)
-c8.place(x=60, y=40)
+c8 = Checkbutton(master, text = "Desactivar", variable=var8)
+c8.place(x=450, y=380)
 
-master.geometry("900x400")
+"""Bloque1 = Label(master, width=75, height = 75, bg = "white", relief="solid")
+Bloque1.place(x = 550, y = 150)
+
+Bloque2 = Label(master, width=75, height = 75, bg = "black", relief="solid")
+Bloque2.place(x = 625, y = 150)
+
+Bloque3 = Label(master, width=75, height = 75, bg = "white", relief="solid")
+Bloque3.place(x = 700, y = 150)
+
+Bloque4 = Label(master, width=75, height = 75, bg = "white", relief="solid")
+Bloque4.place(x = 550, y = 225)
+
+Bloque5 = Label(master, width=75, height = 75, bg = "white", relief="solid")
+Bloque5.place(x = 625, y = 225)
+
+Bloque6 = Label(master, width=75, height = 75, bg = "white", relief="solid")
+Bloque6.place(x = 700, y = 225)
+
+Bloque7 = Label(master, width=75, height = 75, bg = "white", relief="solid")
+Bloque7.place(x = 550, y = 300)
+
+Bloque8 = Label(master, width=75, height = 75, bg = "white", relief="solid")
+Bloque8.place(x = 625, y = 300)
+
+Bloque9 = Label(master, width=75, height = 75, bg = "white", relief="solid")
+Bloque9.place(x = 700, y = 300)"""
+
 master.mainloop()
